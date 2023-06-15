8c02df7a640aaa893675bc446dc0ffdb750d36af
MeruC <carmelomelvincent@gmail.com>
2023-06-15 19:04:25 +0800

    alot

--- Scripts/Player_Input/movement.gd
+++ Scripts/Player_Input/movement.gd
@@ -13,14 +13,49 @@ var velocity := Vector3.ZERO
 var start_pos = Vector3(0, .5, 0)
 
 var can_move = false
+var is_moving = false
+var frame = 0
+var prev
+var timer = null#$Pivot/walk/Timer
+
+onready var idle = $Pivot/CSGSphere
+onready var walk = $Pivot/walk
+onready var walking = [$"Pivot/walk/1", $"Pivot/walk/2", $"Pivot/walk/3", $"Pivot/walk/4", $"Pivot/walk/5", $"Pivot/walk/6"]
+
+func _ready():
+	timer = Timer.new()
+	timer.set_one_shot(false)
+	timer.set_wait_time(0.2)
+	timer.connect("timeout", self, "animation")
+	add_child(timer)
+	timer.start()	
 
 func _physics_process(delta: float) -> void:
 	var input_vector = get_input_vector()
 	var direction = get_direction(input_vector)
 	apply_friction(direction, delta)
 	apply_movement(input_vector, direction, delta)
-	velocity = move_and_slide(velocity, Vector3.UP) 
+	velocity = move_and_slide(velocity, Vector3.UP)
+	yield(get_tree().create_timer(2), "timeout")
 	
+	
+func _process(delta):
+	if velocity.x == 0 and velocity.z == 0:
+		idle.set_visible(true)
+		walk.set_visible(false)
+	else:
+		idle.set_visible(false)
+		walk.set_visible(true)	
+	
+func animation():
+	prev = frame
+	frame += 1
+	if(frame > 5): frame = 0
+	print(prev)
+	print(frame)
+	walking[prev].set_visible(false)
+	walking[frame].set_visible(true)
+
 #Getting The Input
 func get_input_vector():
 	var input_vector := Vector3.ZERO
---
+++ main.gd
@@ -0,0 +1,23 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var timer = null
+var x = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func on_timeout_complete():
+	print(x)
+	x += 1
+	pass
