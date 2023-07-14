f59d113f4fc6e28f2d2e2a3b0947bf5c93dd7d8f
MeruC <carmelomelvincent@gmail.com>
2023-07-14 18:20:23 +0800

    big stuff, level editor inventory, scale settings
    sorry it's messy, I'm gonna fix it later

--- ui/ui_container.gd
+++ ui/ui_container.gd
@@ -1,14 +1,4 @@
 extends Control
 
-export(NodePath) onready var settings = get_node(settings) as Control
-export(NodePath) onready var split_stack = get_node(split_stack) as Control
-
-func _on_settings_pressed():
-	if (split_stack.visible):
-		split_stack.visible = false
-	settings.visible = !settings.visible
-	settings.raise()
-
-
 func _on_exit_pressed():
 	get_tree().quit()
