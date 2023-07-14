2b73258b533c668301afff49f2e714edb7d61d62
MeruC <carmelomelvincent@gmail.com>
2023-07-14 12:35:11 +0800

    Hotbar and Item Stacking

--- managers/item_manager.gd
+++ managers/item_manager.gd
@@ -1,11 +1,7 @@
 extends Node
 
-onready var items = {
-	"tree" : preload("res://global/items/data/tree.tscn"),
-	"stone_brick" : preload("res://global/items/data/stone_brick.tscn"),
-	"gold" : preload("res://global/items/data/gold.tscn"),
-	"crystal" : preload("res://global/items/data/crystal.tscn"),
-}
+const ITEM_PATH = "res://global/items/data/items.json"
+var items = {}
 
 onready var placeholders = {
 	Game_Enums.EQUIPMENT_TYPE.HEAD : preload("res://global/GUI/inventory/sprites/placeholder_head.png"),
@@ -14,8 +10,14 @@ onready var placeholders = {
 	Game_Enums.EQUIPMENT_TYPE.OFFHAND : preload("res://global/GUI/inventory/sprites/placeholder_offhand.png"),
 }
 
-func get_item( id : String ):
-	return items[id].instance()
+func _ready():
+	var file = File.new()
+	file.open( ITEM_PATH, File.READ )
+	items = parse_json( file.get_as_text() )
+	file.close()
 
-func get_placeholder(id):
-	return placeholders[id]
+func get_item( id : String ):
+	return Item.new( id, items[ id ] )
+
+func get_placeholder( id ):
+	return placeholders[ id ]
---
+++ managers/resource_manager.gd
@@ -0,0 +1,50 @@
+class_name Resource_Manager extends Node
+
+const STAT_PATH = "res://global/items/data/stats.json"
+
+var sprites = {
+	"chestplate": preload("res://global/items/sprites/chestplate.png"),
+	"crystal": preload("res://global/items/sprites/crystal.png"),
+	"gold_coin": preload("res://global/items/sprites/gold_coin.png"),
+	"helmet": preload("res://global/items/sprites/helmet.png"),
+	"iron_sword": preload("res://global/items/sprites/iron_sword.png"),
+	"magic_orb": preload("res://global/items/sprites/magic_orb.png"),
+	"shield": preload("res://global/items/sprites/shield.png"),
+	"stone_brick": preload("res://global/items/sprites/stone_brick.png"),
+	"tshirt": preload("res://global/items/sprites/tshirt.png"),
+	"wood": preload("res://global/items/sprites/wood.png"),
+	"wooden_sword": preload("res://global/items/sprites/wooden_sword.png"),
+}
+
+var fonts = {
+	8: preload("res://resources/font/button.tres")
+}
+
+var colors = {
+	"normal": Color( "905c32" )
+}
+
+var tscn = {
+	"splitter": preload("res://ui/splitter.tscn"),
+	"hotbar_slot": preload("res://global/GUI/inventory/hotbar_slot.tscn")
+}
+
+var stat_info = {}
+
+func _ready():
+	var file = File.new()
+	file.open( STAT_PATH, File.READ )
+	var data = parse_json( file.get_as_text() )
+	
+	for stat in data:
+		stat_info[ Game_Enums.STAT[ stat ] ] = data[ stat ]
+	
+	file.close()
+
+
+
+
+
+
+
+
