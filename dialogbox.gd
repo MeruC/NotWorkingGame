eee837b711b49c98c2c45c866c0f1a0c46be752b
SHIR00101 <120690550+SHIR00101@users.noreply.github.com>
2023-08-28 10:05:50 -0700

    finishing touches for level 1 dicussion

--- dialogbox.gd
+++ dialogbox.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 var json_file = "res://offline_levels/json/level1_script.json"
 var json_data = []
 export (float) var textSpeed = 0.05
@@ -31,8 +32,21 @@ func update_dialog():
 		var title = json_data[size]["title"]
 		var content = json_data[size]["content"]
 		$"../CanvasLayer/NinePatchRect/dialog".text = content
-		$"../CanvasLayer/NinePatchRect/title".text = title
+		$"../example1/AnimationPlayer/title".text = title
+		if size == 2:
+			$"../example1/AnimationPlayer/file".visible = true
+			$"../example1/AnimationPlayer/name".visible = true
+			$"../example1/AnimationPlayer/age".visible = true
+			$"../example1/AnimationPlayer".play("file_animation")
+		if size == 3:
+			$"../example1/AnimationPlayer".play("name_animation")
+		if size == 4:
+			$"../example1/AnimationPlayer".play_backwards("file_animation")
+			$"../example1/AnimationPlayer/file".visible = false
+			$"../example1/AnimationPlayer/name".visible = false
+			$"../example1/AnimationPlayer/age".visible = false
 	else:
 		print("Dialog ended.")
 
 	# You can also return json_data here if needed
+
