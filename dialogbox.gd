889a26102fe77a8f7af4c7616c38599f2874a92a
SHIR00101 <120690550+SHIR00101@users.noreply.github.com>
2023-09-09 13:31:27 +0800

    level1_discussion video inserted in level 1 discussion

--- dialogbox.gd
+++ dialogbox.gd
@@ -3,16 +3,19 @@ extends Node2D
 export( NodePath ) onready var VoiceGen = get_node( VoiceGen ) as AudioStreamPlayer
 export( NodePath ) onready var dialog = get_node( dialog ) as Label
 
+
 var json_file = "res://offline_levels/json/level1_script.json"
 var json_data = []
 var textSpeed = 1
 var total_character = 0
 var click = 0
 var game_scene = "res://offline_levels/level1/level_1.tscn"
+var video_plyer: VideoPlayer
 
 var size = 0
 
 func _ready():
+	video_plyer = $"../VideoPlayer"
 	VoiceGen.pitch_scale = 1.5
 	var file = File.new()
 	if file.open(json_file, File.READ) == OK:
@@ -25,8 +28,9 @@ func _ready():
 			print("JSON parsing error:", json_result.error_string)
 	else:
 		print("Failed to open JSON file.")
-
 	update_dialog()
+	
+	video_plyer.connect("finished", self, "_on_video_finished")
 	
 func _process(_delta):
 	if $"../CanvasLayer/NinePatchRect/dialog".visible_characters < total_character:
@@ -40,8 +44,11 @@ func _process(_delta):
 			click = 0
 			size += 1
 			update_dialog()
+
+
 			
 func _input(event):
+	
 	if event is InputEventScreenTouch and event.pressed:
 		click += 1
 		$"../CanvasLayer/NinePatchRect/dialog".visible_characters = total_character
@@ -58,9 +65,8 @@ func update_dialog():
 		$"../CanvasLayer/NinePatchRect/dialog".text = content
 		
 		total_character = content.length()
-		$"../CanvasLayer/NinePatchRect/dialog".visible_characters = 0
 		
-		$"../example1/AnimationPlayer/title"
+		$"../CanvasLayer/NinePatchRect/dialog".visible_characters = 0
 		$"../CanvasLayer/NinePatchRect/title".text = title
 		
 		VoiceGen.read(dialog.text)
@@ -75,16 +81,45 @@ func update_dialog():
 		if size == 4:
 			
 			$"../example1/AnimationPlayer".play("name_animation")
-			
 		if size == 5:
-			
-			$"../example1/AnimationPlayer".play_backwards("file_animation")
+			$"../video".visible = true
+			$"../play_button".visible = true
 			$"../example1/AnimationPlayer/file".visible = false
 			$"../example1/AnimationPlayer/name".visible = false
 			$"../example1/AnimationPlayer/age".visible = false
+			
+		if size == 6:
+			$"../play_button".visible = false
+			$"../video".visible = false
+			$"../cancel_button".visible = false
+			video_plyer.stop()
+			$"../example1/AnimationPlayer".play_backwards("file_animation")
 	else:
 		get_tree().change_scene(game_scene)
 		print("Dialog ended.")
 
 	# You can also return json_data here if needed
 
+func _on_play_button_pressed():
+	$"../video".visible = false
+	$"../VideoPlayer".visible = true
+	$"../cancel_button".visible = true
+	video_plyer.play()
+	video_plyer.volume = 8
+	$"../example1/AnimationPlayer".play("video_zoom")
+	$"../play_button".visible = false
+	click =0
+		
+func _on_VideoPlayer_finished():
+		$"../VideoPlayer".visible = false
+		$"../video".visible = true
+		$"../play_button".visible = true
+
+func _on_cancel_button_pressed():
+		$"../VideoPlayer".visible = false
+		$"../video".visible = true
+		video_plyer.stop()
+		$"../play_button".visible = true
+		$"../cancel_button".visible = false
+		click =0
+		
